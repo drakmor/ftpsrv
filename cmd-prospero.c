3d2c69fa27cd4cd5ef19dc60786ba699b5d0c881
John Törnblom <john.tornblom@gmail.com>
2026-02-03 19:52:51 +0100

    cosmetics

--- cmd-prospero.c
+++ cmd-prospero.c
@@ -92,7 +92,15 @@ ftp_parse_authid(const char *arg, uint64_t *out) {
  **/
 int
 ftp_cmd_MTRW(ftp_env_t *env, const char* arg) {
-
+  struct iovec iov_preinst[] = {
+    IOVEC_ENTRY("from"),      IOVEC_ENTRY("/dev/ssd0.preinst"),
+    IOVEC_ENTRY("fspath"),    IOVEC_ENTRY("/preinst"),
+    IOVEC_ENTRY("fstype"),    IOVEC_ENTRY("exfatfs"),
+    IOVEC_ENTRY("large"),     IOVEC_ENTRY("yes"),
+    IOVEC_ENTRY("timezone"),  IOVEC_ENTRY("static"),
+    IOVEC_ENTRY("async"),     IOVEC_ENTRY(NULL),
+    IOVEC_ENTRY("ignoreacl"), IOVEC_ENTRY(NULL),
+  };
   struct iovec iov_sys[] = {
     IOVEC_ENTRY("from"),      IOVEC_ENTRY("/dev/ssd0.system"),
     IOVEC_ENTRY("fspath"),    IOVEC_ENTRY("/system"),
@@ -102,7 +110,6 @@ ftp_cmd_MTRW(ftp_env_t *env, const char* arg) {
     IOVEC_ENTRY("async"),     IOVEC_ENTRY(NULL),
     IOVEC_ENTRY("ignoreacl"), IOVEC_ENTRY(NULL),
   };
-
   struct iovec iov_sysex[] = {
     IOVEC_ENTRY("from"),      IOVEC_ENTRY("/dev/ssd0.system_ex"),
     IOVEC_ENTRY("fspath"),    IOVEC_ENTRY("/system_ex"),
@@ -113,29 +120,20 @@ ftp_cmd_MTRW(ftp_env_t *env, const char* arg) {
     IOVEC_ENTRY("ignoreacl"), IOVEC_ENTRY(NULL),
   };
 
-  struct iovec iov_preinst[] = {
-  IOVEC_ENTRY("from"),      IOVEC_ENTRY("/dev/ssd0.preinst"),
-  IOVEC_ENTRY("fspath"),    IOVEC_ENTRY("/preinst"),
-  IOVEC_ENTRY("fstype"),    IOVEC_ENTRY("exfatfs"),
-  IOVEC_ENTRY("large"),     IOVEC_ENTRY("yes"),
-  IOVEC_ENTRY("timezone"),  IOVEC_ENTRY("static"),
-  IOVEC_ENTRY("async"),     IOVEC_ENTRY(NULL),
-  IOVEC_ENTRY("ignoreacl"), IOVEC_ENTRY(NULL),
-  };
-
-  if (nmount(iov_sys, IOVEC_SIZE(iov_sys), MNT_UPDATE)) {
-      return ftp_perror(env);
+  if(nmount(iov_preinst, IOVEC_SIZE(iov_preinst), MNT_UPDATE)) {
+    return ftp_perror(env);
+  }
+  if(nmount(iov_sys, IOVEC_SIZE(iov_sys), MNT_UPDATE)) {
+    return ftp_perror(env);
+  }
+  if(nmount(iov_sysex, IOVEC_SIZE(iov_sysex), MNT_UPDATE)) {
+    return ftp_perror(env);
   }
 
-  if (nmount(iov_sysex, IOVEC_SIZE(iov_sysex), MNT_UPDATE)) {
-      return ftp_perror(env);
-  }
-
-  if (nmount(iov_preinst, IOVEC_SIZE(iov_preinst), MNT_UPDATE)) {
-      return ftp_perror(env);
-  }
-
-  return ftp_active_printf(env, "200 /system /system_ex and /preinst remounted\r\n");
+  return ftp_active_printf(env,
+			   "200- /preinst remounted\r\n"
+			   "200- /system remounted\r\n"
+			   "200 /system_ex remounted\r\n");
 }
 
 /**
